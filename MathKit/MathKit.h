bc07450b7b286c57e7c4445b6582cfd2e4f23072
Adam Gastineau <adam@appcannonsoftware.com>
2015-11-24 15:16:27 -0500

    Initial Commit

---
+++ MathKit/MathKit.h
@@ -0,0 +1,19 @@
+//
+//  MathKit.h
+//  MathKit
+//
+//  Created by Adam Gastineau on 11/24/15.
+//  Copyright © 2015 Adam Gastineau. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MathKit.
+FOUNDATION_EXPORT double MathKitVersionNumber;
+
+//! Project version string for MathKit.
+FOUNDATION_EXPORT const unsigned char MathKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MathKit/PublicHeader.h>
+
+
